a0c02d17054fdbc33990f5b682a52d39383104cd
Tae Ji Jang <xowl1596@gmail.com>
2019-12-09 09:18:31 +0900

    2019-12-09 : 기초 DB 구성 완료 -멤버, 게시물, 카테고리 테이블 및 테이블 간 관계 구성 완료

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -16,15 +16,32 @@ INSERT INTO member values('user7', 'pw', 'name7', '7@a.a', '00000000000');
 INSERT INTO member values('user8', 'pw', 'name8', '8@a.a', '00000000000');
 INSERT INTO member values('user9', 'pw', 'name9', '9@a.a', '00000000000');
 
-CREATE TABLE role(
-	role NUMBER,
-	member VARCHAR2(40) NOT NULL
+CREATE TABLE category(
+	id NUMBER PRIMARY KEY,
+	categoryname VARCHAR2(40) NOT NULL
 );
 
+INSERT INTO category VALUES(1, '공지사항');
+INSERT INTO category VALUES(2, '자유게시판');
+INSERT INTO category VALUES(3, '질문과답변');
 
+CREATE TABLE post(
+	id NUMBER AUTO_INCREMENT PRIMARY KEY,
+	title VARCHAR2(100) NOT NULL,
+	category NUMBER NOT NULL,
+	writer VARCHAR2(40) NOT NULL,
+	writedate DATE default sysdate,
+	modifydate DATE default sysdate,
+	content VARCHAR2(1000) NOT NULL,
+	FOREIGN KEY (category) REFERENCES category(id)
+);
 
---CREATE TABLE POST(
---	ID NUMBER PRIMARY KEY,
---	WRITER VARCHAR2(40) NOT NULL,
---	CONTENT VARCHAR2(100) NOT NULL
---);
+INSERT INTO post(title, category, writer, content) VALUES('title1',1,'user1','content1');
+INSERT INTO post(title, category, writer, content) VALUES('title2',2,'user2','content2');
+INSERT INTO post(title, category, writer, content) VALUES('title3',1,'user3','content3');
+INSERT INTO post(title, category, writer, content) VALUES('title4',1,'user2','content4');
+INSERT INTO post(title, category, writer, content) VALUES('title5',2,'user4','content5');
+INSERT INTO post(title, category, writer, content) VALUES('title6',3,'user2','content6');
+INSERT INTO post(title, category, writer, content) VALUES('title7',1,'user7','content7');
+INSERT INTO post(title, category, writer, content) VALUES('title8',2,'user7','content8');
+INSERT INTO post(title, category, writer, content) VALUES('title9',1,'user9','content9');
