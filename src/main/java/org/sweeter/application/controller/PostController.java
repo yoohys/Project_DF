bffbd4491429eebbf873cdd4a7ebddb59380e628
Tae Ji Jang <xowl1596@gmail.com>
2019-12-13 17:07:19 +0900

    2019-12-13 : 게시물 조회 오류 수정 -게시물 목록 가져오기 문제 해결

--- src/main/java/org/sweeter/application/controller/PostController.java
+++ src/main/java/org/sweeter/application/controller/PostController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -18,7 +19,7 @@ public class PostController {
 	
 	@RequestMapping("/posts/{category}/{page}/{count}")
 	@ResponseBody
-	public List<Post> post(int category, int page, int count){
+	public List<Post> post(@PathVariable int category, @PathVariable int page,@PathVariable int count){
 		return postService.getPostList(category, page, count);
 	}
 	
@@ -27,7 +28,7 @@ public class PostController {
 		postService.create(post);
 		int category = post.getCategory();
 		
-		return "/posts"+category+"/1/10";
+		return "/posts/"+category+"/1/10";
 
 	}
 }
